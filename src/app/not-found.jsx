8e74b0704e09d73290ba1f8a7db99b1865e1133d
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2024-01-04 14:57:11 +0700

    feat: add 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import Typewriter from "typewriter-effect";
+
 const Page = () => {
   return (
     <>
-      <h3>Just DAILY UPLOAD</h3>
+      <section className="flex min-h-screen items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-6">
+          <span className="text-color-primary text-xl">
+            404
+          </span>
+          <h3 className="text-color-primary text-xl">
+            <Typewriter
+              skipAddStyles={false}
+              options={{
+                strings: `You caught me, this page is unexistence`,
+                autoStart: true,
+                delay: "natural",
+              }}
+            />
+            {/* You caught me, this page is{" "}
+            <span className="text-color-primary">unexistence</span> */}
+          </h3>
+          <Link
+            href={"/"}
+            className="animate-bounce text-color-slate transition ease-in-out hover:text-color-primary"
+          >
+            <h2 className="text-xl">Take me home! {`>`}:{`(`}</h2>
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
