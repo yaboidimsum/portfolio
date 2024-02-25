fa6d0350f07d7eb698263287fd85adf9f193cd74
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2024-02-25 23:57:27 +0700

    feat: add certi

--- src/constants/index.js
+++ src/constants/index.js
@@ -638,6 +638,16 @@ const certificateContent = [
     firstButtonText: "CERTIFICATE OF COMPLETION",
     secondButtonText: "COURSERA WEBSITE",
   },
+  {
+    src: "/certificate/min/Coursera-Crash Course Python.png",
+    title: "Coursera Google Certification: Using Python to Interact with the Operating System ",
+    technique:
+      "Python, Operating System, File System, Command Line Interface, Python Scripting, Bash Scripting, Unit Testing, Regular Expression, Python Library",
+    firstLink: "https://coursera.org/share/9dfd3ec817e80e7fb62663055e85e620",
+    secondLink: "https://www.coursera.org",
+    firstButtonText: "CERTIFICATE OF COMPLETION",
+    secondButtonText: "COURSERA WEBSITE",
+  },
 ];
 
 const blogContent = [
